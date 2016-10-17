310340c3533682f4258e3af77df510188836592c
Ebube Chuba <ebube97@me.com>
2016-10-17 19:49:16 -0400

    working stage4

--- stage4.py
+++ stage4.py
@@ -4,16 +4,18 @@ import json
 def no_prefix():
   r = requests.post('http://challenge.code2040.org/api/prefix', data={'token': '7628d19dc804169849b04c989c364a4e'})
   d = json.loads(r.text)
-  print(d['prefix'])
-  print(d['array'])
-  return [word for word in d['array'] if word[:len(d['prefix'])] != d['prefix']]
+  prefix = d['prefix']
+  all_words = d['array']
+  print(prefix)
+  print(all_words)
+
+  return [word for word in all_words if word[:len(prefix)] != prefix]
 
 
 if __name__ == '__main__':
   arr = no_prefix()
   clean_arr = json.dumps(arr)
   print(clean_arr)
-  print(json.loads(clean_arr))
-  s = requests.post('http://challenge.code2040.org/api/prefix/validate', data={'token': '7628d19dc804169849b04c989c364a4e', 'array': clean_arr})
+  s = requests.post('http://challenge.code2040.org/api/prefix/validate', json={'token': '7628d19dc804169849b04c989c364a4e', 'array': no_prefix()})
   print(s.status_code)
   print(s.text)
